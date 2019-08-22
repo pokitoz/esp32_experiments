56926d872771b22e842259bf5d2a024e2599fc81
Florian Depraz <florian.depraz@arm.com>
2019-08-22 20:35:16 +0100

    Mainfile as helloworld

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,9 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+
 #include <stdio.h>
+#include "wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
